efa576d1764c2c06823830d86f8264a9b7ceef3e
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-03-11 16:38:08 +0530

    update

---
+++ 0_console.js
@@ -0,0 +1,11 @@
+var playerOne = 120; 
+var playerTwo = 130; 
+var playerThree = 140; 
+var playerFour = 150; 
+var playerFive = 160; 
+console.log("Console.log" + " " + playerOne); 
+console.debug("Console.debug" + " " +playerTwo); 
+console.warn("Console.warn" + " " + playerThree); 
+console.info("Console.info" + " " + playerFour); 
+console.error("Console.error" + " " + playerFive);
+console.log('printf %s %d %f','string',12, 12.12);
---
+++ 1_Let_Const.js
@@ -0,0 +1,11 @@
+console.log('*******LET and Const **********')
+let message;
+console.log(message);
+message="Hello";
+console.log(message);
+
+const PI = 3.14;
+console.log(PI);
+PI= 22/7;
+
+console.log('*****************')
